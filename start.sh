ef25750a2953c163956bc045b7c2021f417aa340
Ralf Geschke <ralf@kuerbis.org>
2017-01-06 09:59:36 +0100

    Fix start process, add generate assets and set Elasticsearch url

--- start.sh
+++ start.sh
@@ -6,6 +6,15 @@ export PATH=/opt/zammad/bin:$PATH
 export GEM_PATH=/opt/zammad/vendor/bundle/ruby/2.3.0/
 
 
+if [ ! -f /opt/zammad/initdata/started ]; then
+        rake assets:precompile
+
+        rails r "Setting.set('es_url', 'http://localhost:9200')"
+
+        echo true > /opt/zammad/initdata/started
+        
+fi
+
 script/websocket-server.rb start &
 script/scheduler.rb start &
 rails s --binding=0.0.0.0 -p 3000
